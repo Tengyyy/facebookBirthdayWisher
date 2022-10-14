e962c1cacfed8bb66264cbc547396df829b1179f
Tengyyy <hansmartintomingas@gmail.com>
2022-10-14 19:33:21 +0300

    facebooki sisse logimine

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+chrome_options = Options()
+chrome_options.add_argument("--disable-notifications")
+chrome_options.add_experimental_option("detach", True)  # jätab akna lahti
 
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+driver.get("https://www.facebook.com/")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+cookies = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.XPATH, '//button[@class="_42ft _4jy0 _9xo6 _4jy3 _4jy1 selected _51sy"]'))).click()
 
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# textfieldidesse kasutaja ja parooli sisestamine
+username.clear()
+username.send_keys("")  # siia tuleb kasutajanimi/mail
+password.clear()
+password.send_keys("")  # siia tuleb parool
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# sisenemise nupu vajutamine
+button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
